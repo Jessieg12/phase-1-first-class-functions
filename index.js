38450c82b4b2cfac44fe1fdcce69fbbc231858b2
Jessie Guzman <jessieguzman777@gmail.com>
2022-11-02 23:03:45 -0500

    complete

---
+++ index.js
@@ -0,0 +1,15 @@
+function receivesAFunction(callback){
+  callback()
+}
+
+function returnsANamedFunction(){
+  return function namedFunction(){
+    return "namedFunction"
+  }
+}
+
+function returnsAnAnonymousFunction(){
+  return () => {
+    return "annonymousFunction"
+  }
+}
